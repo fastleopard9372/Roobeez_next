7e9529a03ccba5b036adf5d9fd241a1727bee812
fastleopard <122797076+vegasnc@users.noreply.github.com>
2024-01-25 00:50:14 +0900

    install swiper

--- pages/Landing.jsx
+++ pages/Landing.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
 const Landing = () => {
   return <div className={styles.container}>Landing</div>;
 };
